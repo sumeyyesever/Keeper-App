ccdc7c68b10524522ccfe11ebcc2f39b0fb415df
sumeyyesever <sumeysever@gmail.com>
2022-11-14 21:26:40 +0300

    complete part 3

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { isPropertySignature } from "typescript";
+
+function CreateArea(props) {
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+    
+    function handleChange(event){
+        const {name, value} = event.target;
+        setNote(prevValue => {
+            return {
+                ...prevValue,
+                [name] : value
+            }
+        });
+    }
+
+    function submitNote(event){
+        props.onAdd(note);
+        setNote({
+            title:"",
+            content:""
+          })
+        event.preventDefault();
+    }
+
+
+
+  return (
+    <div>
+      <form>
+        <input onChange={handleChange} name="title" value={note.title} placeholder="Title" />
+        <textarea onChange={handleChange} name="content" value={note.content} placeholder="Take a note..." rows="3" />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
